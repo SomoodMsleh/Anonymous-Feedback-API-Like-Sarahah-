3215a05423b57f652eb07f9d992347924698f60f
SomoodMsleh <somoodmedwan@gmail.com>
2025-02-13 11:50:04 +0200

    add .env file and eidt rndpoint

--- DB/connection.js
+++ DB/connection.js
@@ -5,7 +5,7 @@ dotenv.config();
 export const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
     host: process.env.DB_HOST,
     port:process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT /* 'mysql' 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+    dialect:'mysql' /*process.env.DB_DIALECT 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
 });
 
 export const connectDB = ()=>{
